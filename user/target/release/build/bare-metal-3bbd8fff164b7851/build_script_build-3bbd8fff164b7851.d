6597c37f41b5a50f31abf83bfa53724ff2e2e1b2
root <root@DESKTOP-8S6MT82.localdomain>
2022-04-19 21:27:44 +0800

    太难了

---
+++ user/target/release/build/bare-metal-3bbd8fff164b7851/build_script_build-3bbd8fff164b7851.d
@@ -0,0 +1,5 @@
+/home/qpr/OS_Competition/user/target/release/build/bare-metal-3bbd8fff164b7851/build_script_build-3bbd8fff164b7851: /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/bare-metal-0.2.5/build.rs
+
+/home/qpr/OS_Competition/user/target/release/build/bare-metal-3bbd8fff164b7851/build_script_build-3bbd8fff164b7851.d: /root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/bare-metal-0.2.5/build.rs
+
+/root/.cargo/registry/src/mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd/bare-metal-0.2.5/build.rs:
---
+++ user/target/riscv64gc-unknown-none-elf/debug/priv_inst.d
@@ -0,0 +1,1 @@
+/home/qpr/OS_Competition/user/target/riscv64gc-unknown-none-elf/debug/priv_inst: /home/qpr/OS_Competition/user/src/bin/priv_inst.rs /home/qpr/OS_Competition/user/src/console.rs /home/qpr/OS_Competition/user/src/lang_items.rs /home/qpr/OS_Competition/user/src/lib.rs /home/qpr/OS_Competition/user/src/syscall.rs
