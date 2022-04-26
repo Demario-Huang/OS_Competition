28323b07744b36af815b898b524393bfaa893bd7
Demario-Huang <119010108@link.cuhk.edu.cn>
2022-04-26 22:50:02 +0800

    demo framealloc

--- kernel/include/framealloc.h
+++ kernel/include/framealloc.h
@@ -1,17 +1,14 @@
 #ifndef __FRAMEALLOC_H
 #define __FRAMEALLOC_H
 
-#define PAGE_SIZE 4096 // 4kb size for each page and frame
+
+// Here the physical kernel space is 8MB.
+// contains  2k frame, for each frame 4kb size
+// I implement the frame allocation by using bitmap, the bitmap is 2k bit, whic is half of one frame.
+
 #include "types.h"
 
-extern uint64 get_frame(void); // return the first address of current free frame
-extern void free_frame(uint64 frame_addr);
-
-struct frame {
-    uint64 addr;
-    uint32 frame_id;
-    struct frame *next;
-};
-
+extern uint32 get_frame(); // return the first address of current free frame
+extern void free_frame(uint32 frame_number);
 
 #endif // FRAMEALLOC_H
--- kernel/src/main.c
+++ kernel/src/main.c
@@ -7,12 +7,14 @@
 #include "types.h"
 #include "loader.h"
 #include "proc.h"
+#include "framealloc.h"
 
 extern uint64 app_0_start;
 extern uint64 app_0_end;
 
 void main(int num_core) {
     printf("[kernel] num of cores: %d \n", num_core);  // 通过寄存器entry.S中设置的寄存器a0来传这个num_core参数
+    uint32 a = get_frame();
     load();    // 将应用程序load到主内存中
     init_app(); // 初始化App，包括初始化其trap上下文，将trap上下文放到用户栈中。
     run_app();
