742c039280e3d7cf3cf3c2a5fe3e8eeb83b23306
QinPR <1905873179@qq.com>
2022-05-30 15:06:51 +0800

    proceed in task manager

--- kernel/src/task/task_context.d
+++
@@ -1,3 +0,0 @@
-kernel/src/task/task_context.o: kernel/src/task/task_context.c \
- kernel/include/task_context.h kernel/include/types.h \
- kernel/include/types.h
--- kernel/src/trap/trap_context.d
+++
@@ -1,3 +0,0 @@
-kernel/src/trap/trap_context.o: kernel/src/trap/trap_context.c \
- kernel/include/trap_context.h kernel/include/types.h \
- kernel/include/types.h
